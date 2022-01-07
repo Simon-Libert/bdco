0166d9b14a3bb26c5cabc62f2603a4776a3b5c36
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-01-07 14:27:34 +0100

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import './styles/index.scsss';
+import './styles/index.scss';
 
 ReactDOM.render(
 	<React.StrictMode>
