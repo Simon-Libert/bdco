25680c97a34e98c4b19069fbd2dfbda9f5db953f
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-01-10 15:53:35 +0100

    signup, sign in, fix files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Profil from './pages/Profil';
 
-export default function App() {
-	return <div></div>;
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/home' element={<Home />} />
+				<Route path='/profil' element={<Profil />} />
+				<Route path='*' element={<Navigate replace to='/home' />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default App;
