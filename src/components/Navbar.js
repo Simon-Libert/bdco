8f017832c8e69421f88a52573b13a034435ab88f
Simon-Libert <91894559+Simon-Libert@users.noreply.github.com>
2022-01-12 10:49:58 +0100

    add svg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import { UidContext } from '../components/AppContext';
 
 const Navbar = () => {
+	const uid = useContext(UidContext); // récupère l'id du user s'il est co
+
 	return (
 		<nav>
 			<div className='Nav-container'>
 				<div className='logo'>
-					<NavLink to='/'>
+					<NavLink exact to='/'>
 						<div className='logo'>
-							<img src='./img/logo_small.png' alt='icon' />
+							<img src='./img/logo_small.png' alt='logo' />
 							<h3>BDCO</h3>
 						</div>
 					</NavLink>
 				</div>
+				{uid ? (
+					<ul>
+						<li></li>
+						<li className='welcome'>
+							<NavLink exact to='/profil'>
+								<h3>Bienvenue {uid}</h3>
+							</NavLink>
+						</li>
+						Logo logout
+					</ul>
+				) : (
+					<ul>
+						<li></li>
+						<li>
+							<NavLink exact to='/profil'>
+								<img src='./img/icons/login.svg' alt='login' />
+							</NavLink>
+						</li>
+					</ul>
+				)}
 			</div>
 		</nav>
 	);
